0d4c989b883d1bcdaeaf5d367ef50661a20ee4b8
binllionaire <bug20474@gmail.com>
2023-10-31 15:07:38 +0900

    practice : 👊🏻 3차 세미나 실습

---
+++ week3/practice/vite-project/src/components/Counter.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+import styled from "styled-components"
+
+const Counter = ({name}) =>{
+    const [number,setNumber] = useState(0);
+    const increaseNumber = () =>{
+        setNumber(number+1)
+    }
+    const decreaseNumber = () =>{
+        setNumber(number-1)
+    }
+    return <Container>
+        <h1>{name}의 계산기</h1>
+        <p>{number}</p>
+        <ButtonContainer>
+            <CalcButton type="button" onClick={increaseNumber}>+1</CalcButton>
+            <CalcButton type="button" onClick={decreaseNumber}>-1</CalcButton>
+        </ButtonContainer>
+    </Container>
+}
+
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100vw;
+  height: 100vh;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  gap: 2px;
+`;
+
+const CalcButton = styled.button`
+  background-color: pink;
+  cursor: pointer;
+
+  &:hover {
+    background-color: blue;
+    color: white;
+  }
+`;
+
+export default Counter;
