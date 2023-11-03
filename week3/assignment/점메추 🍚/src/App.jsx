7a9c5fa1cc1c887a308379df9bac439700585d69
binllionaire <bug20474@gmail.com>
2023-11-03 21:50:29 +0900

    feat : ✨ 추천 방식 페이지 퍼블리싱

--- week3/assignment/점메추 🍚/src/App.jsx
+++ week3/assignment/점메추 🍚/src/App.jsx
@@ -1,35 +1,92 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import mainCharacter from "./assets/img/character.png";
 import "./App.css";
+import { Header, Section } from "./styles/globalStyle";
+import { styled } from "styled-components";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const Title = styled.h1`
+    font-size: 2rem;
+  `;
+  const Quetion = styled.h2`
+    font-size: 3rem;
+    font-weight: 100;
+  `;
+  const Character = styled.img`
+    width: 150px;
+  `;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  const Balloon = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #fff;
+    color: #000;
+    padding: 10px;
+    border-radius: 20px; /* 더 큰 값으로 설정하여 끝을 뽀죡하게 만듭니다. */
+    width: 200px;
+    height: 200px;
+
+    margin: 20px;
+    cursor: pointer;
+
+    &::before {
+      content: "";
+      position: absolute;
+      top: 100%;
+      left: 50%;
+      margin-left: -10px; /* 가운데 정렬을 위해 가로 마진을 조정합니다. */
+      border-width: 10px;
+      border-style: solid;
+      border-color: #fff transparent transparent transparent;
+    }
+
+    &:hover {
+      box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3),
+        0 15px 12px rgba(0, 0, 0, 0.22);
+    }
+  `;
+
+  const BalloonText = styled.p`
+    color: #000;
+    font-size: 1.5rem;
+  `;
+  const Answer = styled.div`
+    display: flex;
+    justify-content: center;
+
+    padding: 50px 0 0;
+  `;
+
+  const [page, setPage] = useState(0);
+  const nextPage = () => {
+    setPage(page + 1);
+  };
+  {
+    switch (page) {
+      case 0:
+        return (
+          <>
+            <Header>
+              <Title>오늘의 점메추</Title>
+            </Header>
+            <Section>
+              <Quetion>원하는 추천 방식을 골라줘!</Quetion>
+              <Answer>
+                <Balloon onClick={() => nextPage()}>
+                  <BalloonText>취향대로 추천</BalloonText>
+                </Balloon>
+                <Balloon onClick={() => nextPage()}>
+                  <BalloonText>랜덤 추천</BalloonText>
+                </Balloon>
+              </Answer>
+              <Character src={mainCharacter} />
+            </Section>
+          </>
+        );
+    }
+  }
 }
 
 export default App;
