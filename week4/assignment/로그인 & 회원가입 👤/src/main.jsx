cad65d67132b57cd343ed151c0a1c346f4cb7121
binllionaire <bug20474@gmail.com>
2023-11-14 19:41:10 +0900

    design : 💄 전역 스타일 적용

--- week4/assignment/로그인 & 회원가입 👤/src/main.jsx
+++ week4/assignment/로그인 & 회원가입 👤/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import GlobalStyle from "./styles/gloabalStyle";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
---
+++ week4/assignment/로그인 & 회원가입 👤/src/styles/gloabalStyle.js
@@ -0,0 +1,63 @@
+import { createGlobalStyle } from "styled-components";
+
+const globalStyle = createGlobalStyle`
+* {
+    box-sizing : border-box;
+}
+
+#root {
+  max-width: 768px;
+  margin:0 auto;
+}
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, canvas, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+button{
+  border: none;
+  cursor:pointer;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+input{
+  border:none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+`;
+export default globalStyle;
