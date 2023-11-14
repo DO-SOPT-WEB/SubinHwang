78227d90f67fbd7feb224a707ec25a8cd059f22b
binllionaire <bug20474@gmail.com>
2023-11-15 07:25:14 +0900

    feat : ✨ 마이페이지 구현

--- week4/assignment/로그인 & 회원가입 👤/src/pages/MyPage.jsx
+++ week4/assignment/로그인 & 회원가입 👤/src/pages/MyPage.jsx
@@ -1,14 +1,39 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
 import { My } from "../styles/commonStyle";
 import profileImg from "../assets/imgs/profile.png";
 
 const MyPage = () => {
+  const location = useLocation();
+
+  const [id, setID] = useState("");
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const path = location.pathname;
+      const idFromURL = path.substring(path.lastIndexOf("/") + 1);
+      try {
+        const data = await axios.get(
+          `http://3.39.54.196/api/v1/members/${idFromURL}`
+        );
+        setID(data.data.username);
+        setName(data.data.nickname);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [location]);
+
   return (
     <My.Wrapper>
       <My.Title>마이페이지</My.Title>
       <My.ProfileImg $profileImg={profileImg}></My.ProfileImg>
       <My.InfoWrapper>
-        <My.InfoText>ID : </My.InfoText>
-        <My.InfoText>닉네임 : </My.InfoText>
+        <My.InfoText>ID : {id}</My.InfoText>
+        <My.InfoText>닉네임 : {name}</My.InfoText>
       </My.InfoWrapper>
       <My.LogoutButton>로그아웃</My.LogoutButton>
     </My.Wrapper>
