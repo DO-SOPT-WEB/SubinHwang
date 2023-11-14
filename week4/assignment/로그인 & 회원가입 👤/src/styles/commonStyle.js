771b70000b6971da3e9650e5d85859a5f876f471
binllionaire <bug20474@gmail.com>
2023-11-15 00:07:22 +0900

    refactor : ♻️  스타일 컴포넌트 분리

---
+++ week4/assignment/로그인 & 회원가입 👤/src/styles/commonStyle.js
@@ -0,0 +1,82 @@
+import { styled } from "styled-components";
+const Title = styled.h1`
+  margin-bottom: 20px;
+  font-size: ${({ theme }) => theme.fontSize.l};
+  color: ${({ theme }) => theme.colors.darkBlue};
+`;
+const InputWrapper = styled.div`
+  width: 90%;
+  height: 60px;
+
+  display: flex;
+  gap: 10px;
+  padding: 0 20px;
+  border-radius: 16px;
+
+  background-color: ${({ theme }) => theme.colors.lightBlue};
+`;
+const InputIcon = styled.img``;
+const Input = styled.input`
+  flex-grow: 1;
+
+  border-radius: 16px;
+  outline: none;
+
+  background-color: ${({ theme }) => theme.colors.lightBlue};
+
+  font-size: ${({ theme }) => theme.fontSize.m};
+  font-family: "Pretendard-SemiBold";
+`;
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+
+  width: 100%;
+  max-width: 500px;
+  height: 100vh;
+
+  margin: 0 auto;
+  padding: 20px;
+
+  background-color: ${({ theme }) => theme.colors.white};
+`;
+const Button = styled.button`
+  width: 90%;
+  height: 60px;
+
+  margin: 0 auto;
+
+  border-radius: 16px;
+
+  background-color: ${({ theme }) => theme.colors.darkBlue};
+  color: ${({ theme }) => theme.colors.white};
+  font-size: ${({ theme }) => theme.fontSize.m};
+  font-weight: 700;
+`;
+const OthersiseWrapper = styled.div`
+  display: flex;
+  gap: 5px;
+
+  font-size: ${({ theme }) => theme.fontSize.s};
+  color: ${({ theme }) => theme.colors.darkBlue};
+  font-weight: 500;
+
+  cursor: pointer;
+`;
+const Otherwise = styled.span`
+  font-weight: 700;
+`;
+
+export const Sign = {
+  Title,
+  InputWrapper,
+  InputIcon,
+  Input,
+  Wrapper,
+  Button,
+  OthersiseWrapper,
+  Otherwise,
+};
