f295b35daa1ba211a039046e8b2200f9f9daa059
binllionaire <bug20474@gmail.com>
2023-11-14 22:44:35 +0900

    design : 💄 ThemeProvider 설정

---
+++ week4/assignment/로그인 & 회원가입 👤/src/styles/theme.js
@@ -0,0 +1,18 @@
+const colors = {
+  darkBlue: "#242b45",
+  lightBlue: "#f1f4ff",
+  lightGrey: "#f5f5f5",
+  white: "#ffffff",
+};
+
+const fontSize = {
+  s: "0.8rem",
+  m: "1rem",
+  l: "1.5rem",
+};
+
+const theme = {
+  colors,
+  fontSize,
+};
+export default theme;
