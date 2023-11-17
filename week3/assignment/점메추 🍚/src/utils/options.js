2ef1e8ab3528b590e95fd3dafc99df01855c98aa
binllionaire <bug20474@gmail.com>
2023-11-17 23:58:21 +0900

    move : 🚚 상수 파일 분리

---
+++ week3/assignment/점메추 🍚/src/utils/options.js
@@ -0,0 +1,16 @@
+export const regionOptions = [
+  { id: "korean", label: "한식" },
+  { id: "western", label: "양식" },
+  { id: "asian", label: "일/중식" },
+];
+
+export const amountOptions = [
+  { id: "largely", label: "거하게" },
+  { id: "properly", label: "적당하게" },
+  { id: "simply", label: "간단하게" },
+];
+
+export const tasteOptions = [
+  { id: "fat", label: "기름진 맛" },
+  { id: "plain", label: "담백한 맛" },
+];
---
+++ week3/assignment/점메추 🍚/src/utils/recommendType.js
@@ -0,0 +1,4 @@
+export const RECOMMEND_BY = {
+  TYPE: "취향대로 추천",
+  RANDOM: "랜덤 추천",
+};
